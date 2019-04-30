9788b7ed8b532efad3df558f961740a50019b243
Rafael Moraes <roliveira.moraes@gmail.com>
2019-04-30 10:14:23 -0300

    Configures the SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,17 @@
+SimpleCov.start do
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
-  parallelize(workers: :number_of_processors)
+
+  # If uncomment the simplecov stop to work
+  # parallelize(workers: :number_of_processors)
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
